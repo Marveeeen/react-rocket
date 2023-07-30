6aeebb9991add45a186095a9f78ebc0903c2f276
Marveeen <jmvroqueee@gmail.com>
2023-07-30 16:42:46 +0800

    style: minor adjustment for tall device

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       screens: {
         'widescreen': { 'raw': "(min-aspect-ratio: 3/2)" },
-        'tallscreen': { 'raw': "(min-aspect-ratio: 1/2)" },
+        'tallscreen': { 'raw': "(max-aspect-ratio: 1/2)" },
       },
       keyframes: {
         'open-menu': {
