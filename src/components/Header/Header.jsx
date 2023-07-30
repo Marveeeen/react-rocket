9cf4b8be8910147413d6a4388d693b1e36953f8a
Marveeen <jmvroqueee@gmail.com>
2023-07-30 16:30:29 +0800

    feature: added mobile navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation";
+import MobileNavigation from "./MobileNavigation";
 
 function Header() {
+  const [showMobileNavigation, setShowMobileNavigation] = useState(false);
+
+  const handleToggleShowMobileNavigation = () => {
+    setShowMobileNavigation((prev) => !prev);
+  };
   return (
     <header className="bg-teal-700 text-white sticky top-0 z-10">
       <section className="max-w-4xl mx-auto p-4 flex justify-between items-center">
         <h1 className="text-3xl font-medium">
           <a href="#hero">🚀 Acme Rockets</a>
         </h1>
-        <Navigation />
+        <Navigation
+          showMobileNavigation={showMobileNavigation}
+          onToggleShowMobileNavigation={handleToggleShowMobileNavigation}
+        />
+      </section>
+      <section
+        className={`absolute top-68 bg-black w-full text-5xl flex-col justify-content-center origin-top animate-open-menu ${
+          showMobileNavigation ? "flex" : "hidden"
+        }`}
+        onClick={handleToggleShowMobileNavigation}
+      >
+        <MobileNavigation />
       </section>
     </header>
   );
---
+++ src/components/Header/MobileNavigation.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function MobileNavigation() {
+  return (
+    <nav
+      className="flex flex-col min-h-screen items-center py-8"
+      aria-label="mobile"
+    >
+      <a href="#hero" className="w-full text-center py-6 hover:opacity-90">
+        Home
+      </a>
+      <a href="#rockets" className="w-full text-center py-6 hover:opacity-90">
+        Our Rockets
+      </a>
+      <a
+        href="#testimonials"
+        className="w-full text-center py-6 hover:opacity-90"
+      >
+        Testimonials
+      </a>
+      <a href="#contact" className="w-full text-center py-6 hover:opacity-90">
+        Contact Us
+      </a>
+      <a href="#footer" className="w-full text-center py-6 hover:opacity-90">
+        Legal
+      </a>
+    </nav>
+  );
+}
+
+export default MobileNavigation;
